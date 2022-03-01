1cc21e71a707793d84e304aa32ec394c5b32c149
Jean <jean.carlosmolossi@gmail.com>
2022-03-01 16:16:38 -0300

    atualizada home para aparecer mais boards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,8 @@ export const Home = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.boardContainer}>
+				<h2>TO DO</h2>
+
 				<Card
 					title="Card"
 					author="John Doe"
@@ -56,6 +58,46 @@ export const Home = () => {
 					comments={1}
 					lastComments={[
 						"https://randomuser.me/api/portraits/women/41.jpg",
+					]}
+				/>
+			</div>
+
+			<div className={styles.boardContainer}>
+				<h2>Doing</h2>
+
+				<Card
+					title="Card"
+					author="John Doe"
+					createdAt={new Date().toISOString()}
+					attachments={[
+						{ type: "file", attachment: "docs.google.com" },
+						{ type: "image", attachment: "docs.google.com" },
+						{ type: "link", attachment: "docs.google.com" },
+					]}
+					categories={[
+						{ type: "neutral", text: "Design" },
+						{ type: "success", text: "Development" },
+						{ type: "warning", text: "UML" },
+					]}
+					comments={1}
+					lastComments={[
+						"https://randomuser.me/api/portraits/women/41.jpg",
+					]}
+				/>
+			</div>
+
+			<div className={styles.boardContainer}>
+				<h2>Done</h2>
+
+				<Card
+					title="Card"
+					author="John Doe"
+					createdAt={new Date().toISOString()}
+					image="https://cdn.pixabay.com/photo/2021/11/20/23/53/desert-6812927_960_720.jpg"
+					categories={[
+						{ type: "danger", text: "Backend" },
+						{ type: "info", text: "Architectural" },
+						{ type: "highlight", text: "Deploy" },
 					]}
 				/>
 			</div>

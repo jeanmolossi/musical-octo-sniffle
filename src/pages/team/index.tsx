446e0b063298f7c45363f352a3a2b097f76e205a
Jean <jean.carlosmolossi@gmail.com>
2022-03-01 14:28:01 -0300

    criada pagina de teammattes

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,6 +1,30 @@
 import React from "react";
+import team from "../../config/users.json";
+import { RenderIf } from "../../helpers/render-if";
 import styles from "./styles.module.scss";
 
 export const Team = () => {
-	return <div className={styles.container}>Team</div>;
+	const renderPhoto = (photo?: string) => {
+		if (!photo) {
+			return "https://randomuser.me/api/portraits/lego/1.jpg";
+		}
+
+		return photo;
+	};
+
+	return (
+		<div className={styles.container}>
+			<h1>Team</h1>
+
+			<div className={styles.team}>
+				{team.map((user) => (
+					<div className={styles.user} key={user.id}>
+						<img src={renderPhoto(user.photo)} alt={user.user} />
+						<div className={styles.name}>{user.user}</div>
+						<div className={styles.role}>{user.username}</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };

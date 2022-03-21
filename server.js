878905fb7793f426c9c39d03a8956de567b9c428
Jean <jean.carlosmolossi@gmail.com>
2022-03-20 23:56:34 -0300

    adicionado fallback

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const express = require('express')
-// const fallback = require('express-history-api-fallback');
+const fallback = require('express-history-api-fallback');
 
 const app = express();
 
 const root = path.join(__dirname, 'public');
 
 app.use(express.static(root));
-// app.use(fallback('index.html', { root }));
+app.use(fallback('index.html', { root }));
 
 app.listen(process.env.PORT || 80, () => {
 	console.log('Server started at port ' + (process.env.PORT || 80));
